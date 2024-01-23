2832903936ea0ba80f5cd79c68144400acf6e1da
Marcmuu <morellgumi@gmail.com>
2024-01-23 16:44:16 +0100

    un poco responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Kanban } from "./components/kanban/Kanban";
 
 function App() {
   return (
-    <div style={{ padding: "50px" }}>
+    <div style={{ padding: "50px" }} className="contenedor">
       <h1 style={{ marginButton: "20px", textAlign: "center" }}>Kanban UI</h1>
       <Kanban />
     </div>
